e02395381a9c9c46f66d2011ad3c4f2f48324e63
M47781 <guezzoulmouad7@gmail.com>
2026-05-14 17:34:13 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: '/eduligne',
-  assetPrefix: '/eduligne/',
+  // basePath: '/eduligne',
+  // assetPrefix: '/eduligne/',
   images: {
     unoptimized: true, // We use this to simplify the <Image> refactor
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   }
 };
 
