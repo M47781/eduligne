65d064c503fc737eeb1aacb53782c18beaaa650f
M47781 <guezzoulmouad7@gmail.com>
2026-05-14 16:20:40 +0100

    update

--- src/app/student/stem/hope-arm/page.tsx
+++ src/app/student/stem/hope-arm/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import React, { useState, useEffect, useRef } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from 'motion/react';
 import { 
   Shield, Award, CheckCircle2, 
   Search, Wind, Lightbulb, Upload, 
